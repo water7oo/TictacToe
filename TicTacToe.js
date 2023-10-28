fc72ab4004508d2432e60e9fc251208b6a69f5d1
water7oo <watersevens37@gmail.com>
2023-10-28 02:26:30 -0400

    Added Game Logic

--- TicTacToe.js
+++ TicTacToe.js
@@ -7,10 +7,13 @@
 //(gameBoard, displayController), use a module.
 //If you need multiples of something (players!), create them with factories.
 
+//Make a button that creates the gameBoard
+
 const createBoard = () => {
   const board = [];
   const boardSize = 3;
   const boardSpace = document.getElementById("boardSpace");
+  let currentPlayer = playerOne;
 
   for (let i = 0; i < boardSize; i++) {
     const row = [];
@@ -21,6 +24,20 @@ const createBoard = () => {
       cell.dataset.col = j;
       row.push(cell);
       boardSpace.appendChild(cell);
+
+      //When cell is clicked it will check to see if a cell
+      //hasn't already had a move made on it
+      //then if fires the makeMove function then makes the
+      //Current player variable equal
+      cell.addEventListener("click", () => {
+        if (!cell.textContent) {
+          makeMove(cell, currentPlayer);
+          if (winCon(currentPlayer.name, currentPlayer.marker)) {
+            console.log(`${currentPlayer.name} wins!`);
+          }
+          currentPlayer = currentPlayer === playerOne ? playerTwo : playerOne;
+        }
+      });
     }
     board.push(row);
   }
@@ -28,14 +45,60 @@ const createBoard = () => {
   return board;
 };
 
-const playerOne = {
-  name: "Player 1",
-  marker: "X",
+//Click the cell --> Checks if cell does not contain content
+//Sets that cells content to the players marker
+const makeMove = (cell, player) => {
+  if (!cell.textContent) {
+    cell.textContent = player.marker;
+  }
 };
 
-const playerTwo = {
-  name: "Player 2",
-  marker: "X",
+//Selects all the cells in the board with the cell class
+const clearCells = () => {
+  const cells = document.querySelectorAll(".cell");
+  cells.forEach((cell) => {
+    cell.textContent = "";
+  });
 };
+
+//Factory that stores the players information
+function createPlayer(name, marker) {
+  return {
+    name: name,
+    marker: marker,
+  };
+}
+
+//Use the createPlayer template to make players 1 and 2
+const playerOne = createPlayer("Player 1", "X");
+const playerTwo = createPlayer("Player 2", "O");
 
 const gameBoard = createBoard();
+
+const winCon = (name, marker) => {
+  const cells = document.querySelectorAll(".cell");
+
+  const possWin = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  for (const combo of possWin) {
+    const [a, b, c] = combo;
+    if (
+      cells[a].textContent === marker &&
+      cells[b].textContent === marker &&
+      cells[c].textContent === marker
+    ) {
+      return true;
+    }
+  }
+
+  return false;
+};
